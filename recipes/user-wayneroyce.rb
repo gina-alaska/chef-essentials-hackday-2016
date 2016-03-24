022509511136a3ae38562fa917b80d74eb2589a6
wayneroyce <whroyce@alaska.edu>
2016-03-24 14:45:31 -0800

    add user whroyce

---
+++ recipes/user-wayneroyce.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: chef-essentials-hackday-2016
+# Recipe:: user-wayneroyce
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+user 'whroyce' do
+  comment 'wayne royce'
+  password 'P@ssw0rd'
+end
